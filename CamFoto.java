8188393e4f9581396b5a165989a0e8a06fe4d945
SpyingRaptor01 <ros21313@uvg.edu.gt>
2021-11-07 11:11:30 -0600

    Interfaz e implements
    Interfaces de las funciones de los productos y las implementaciones en las clases correspondientes 🍕

--- CamFoto.java
+++ CamFoto.java
@@ -1,3 +1,21 @@
-public class CamFoto {
+public class CamFoto extends Producto implements TomarFoto, Reproductor, Portable{
+
+    @Override
+    public String[] mostrarPortable() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String[] reproducirVideo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String[] fotografiar() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }

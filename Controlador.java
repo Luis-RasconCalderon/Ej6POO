5b273acb91075f816225cfa54340cd939dc220cd
SpyingRaptor01 <ros21313@uvg.edu.gt>
2021-11-09 21:11:27 -0600

    Comportamiento a probar para todos los dispositivos

--- Controlador.java
+++ Controlador.java
@@ -60,14 +60,156 @@ public class Controlador{
                         }
                     }
                 }else if(producto == 2){
-
-                }
+                    TelefonoCelular tc = new TelefonoCelular(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(tc);
+                    }else if(op == 2){
+                        int i = 10;
+                        while( i !=0 ){
+                            int probar = vs.probarTelefonoCelular();
+                            if(probar == 1){
+                                String numero = vs.numeroAllamar();
+                                vs.emitirMensaje(tc.hacerLlamada(numero));
+                            }else if(probar == 2){
+                                vs.emitirMensaje(tc.mostrarPortable());
+                            }else if(probar == 3){
+                                i = 0;
+                        }
+                    }       
+                }else if(producto == 3){
+                    TeleFijo tf = new TeleFijo(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(tf);
+                    }else if(op == 2){
+                        int j = 10;
+                        while( j !=0 ){
+                            int probar = vs.probarTelefonoFijo();
+                            if(probar == 1){
+                                String numero = vs.numeroAllamar();
+                                vs.emitirMensaje(tc.hacerLlamada(numero));
+                            }else if(probar == 2){
+                                j = 0;
+                            }
+                        }
+                    }
+                }else if(producto == 4){
+                    CamFoto ca = new CamFoto(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(ca);
+                    }else if(op == 2){
+                }int k = 10;
+                while( k !=0 ){
+                    int probar = vs.probarCamara();
+                    if(probar == 1){
+                                vs.emitirMensaje(ca.fotografiar());
+                            }else if(probar == 2){
+                                vs.emitirMensaje(ca.reproducirVideo());
+                            }else if(probar == 3){
+                                vs.emitirMensaje(ca.mostrarPortable());
+                            }else if(probar == 4){
+                                k = 0;
+                            }
+                        }
+                }else if(producto == 5){
+                    CompPersonalDesk desk = new CompPersonalDesk(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(desk);
+                    }else if(op == 2){
+                }int l = 10;
+                while( l !=0 ){
+                    int probar = vs.probarDesk();
+                    if(probar == 1){
+                        String sitio = vs.introducirSitio();
+                                vs.emitirMensaje(desk.navegadorInternet(sitio));
+                            }else if(probar == 2){
+                                vs.emitirMensaje(desk.reproducirVideo());
+                            }else if(probar == 3){
+                                vs.emitirMensaje(desk.reproducirVideo());
+                            }else if(probar == 4){
+                                l = 0;
+                            }
+                        }
+                }else if(producto == 6){
+                    CompPersonalLap lap = new CompPersonalLap(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(lap);
+                    }else if(op == 2){
+                }int m = 10;
+                while( m !=0 ){
+                    int probar = vs.probarSmarPhone();
+                    if(probar == 1){
+                        String sitio = vs.introducirSitio();
+                                vs.emitirMensaje(lap.navegadorInternet(sitio));
+                            }else if(probar == 2){
+                                vs.emitirMensaje(lap.mostrarPortable());
+                            }else if(probar == 3){
+                                vs.emitirMensaje(lap.reproducirVideo());
+                            }else if(probar == 5){
+                                m = 0;
+                            }
+                        }
+                }else if(producto == 7){
+                    SmartTV tv = new SmartTV(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(tv);
+                    }else if(op == 2){
+                }int n = 10;
+                while( n !=0 ){
+                    int probar = vs.probarSmartTV();
+                    if(probar == 1){
+                        String sitio = vs.introducirSitio();
+                                vs.emitirMensaje(tv.navegadorInternet(sitio));
+                            }else if(probar == 2){
+                                vs.emitirMensaje(tv.reproducirVideo());
+                            }else if(probar == 3){
+                                n = 0;
+                            }
+                        }
+                }else if(producto == 8){
+                    Tablet t = new Tablet(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(t);
+                    }else if(op == 2){
+                }int o = 10;
+                while( o !=0 ){
+                    int probar = vs.probarTablet();
+                    if(probar == 1){
+                        String sitio = vs.introducirSitio();
+                                vs.emitirMensaje(t.navegadorInternet(sitio));
+                            }else if(probar == 2){
+                                vs.emitirMensaje(t.reproducirVideo());
+                            }else if(probar == 3){
+                                vs.emitirMensaje(t.fotografiar());
+                            }else if(probar == 4){
+                                vs.emitirMensaje(t.mostrarPortable());
+                            }else if(probar == 4){
+                                o = 0;
+                            }
+                        }
+                }else if(producto == 9){
+                    SmartWatch sw = new SmartWatch(prodSucursal.get(producto)[0], prodSucursal.get(producto)[1], prodSucursal.get(producto)[2], prodSucursal.get(producto)[3], prodSucursal.get(producto)[4], prodSucursal.get(producto)[5]);
+                    if(op == 1){
+                        carrito.add(sw);
+                    }else if(op == 2){
+                }int p = 10;
+                while( p !=0 ){
+                    int probar = vs.probarTablet();
+                    if(probar == 1){
+                        vs.emitirMensaje(sw.fotografiar());
+                            }else if(probar == 2){
+                                vs.emitirMensaje(sw.mostrarPortable());
+                            }else if(probar == 3){
+                                String numero = vs.numeroAllamar();
+                                vs.emitirMensaje(sw.hacerLlamada(numero));
+                            }else if(probar == 4){    
+                                p = 0;
+                            }
+                        }
+                    
             }else if(menu == 2){
                 vs.mostrarCarrito(carrito);
             }else if(menu == 3){
                 //Aqui se cierra el programa con un System.exit(0);
             }
         }
-    }
     
-}
--- Vista.java
+++ Vista.java
@@ -3,33 +3,33 @@ import java.util.Scanner;
 
 public class Vista {
     Scanner sc = new Scanner(System.in);
-
+//Mensaje de bienvenida al usuario
     public void bienvenida(){
         System.out.println("\n-----------------------------------------------");
         System.out.println("Bienvenido al Electronica Latinoamericana!");
         System.out.println("\n-----------------------------------------------n");
     }
-
+//Mensaje de despedida al salir del programa
     public void despedida(){
         System.out.println("\n-----------------------------------------------");
         System.out.println("Nos vemos pronto!");
         System.out.println("\n-----------------------------------------------");
     }
 
-
+//Preguntar a que numero decea llamar a la hora de probar llamadas
     public String numeroAllamar(){
         System.out.println("\nIngrese el numero de telefono a llamar: ");
         String nume = sc.nextLine();
         return nume;
     }
 
-
+//Preguntar a que sitio desea ingresar el usuario al probar navegador
     public String introducirSitio(){
         System.out.println("\nIngrese el sitio a visitar: ");
         String sitio = sc.nextLine();
         return sitio;
     }
-
+//Preguntar que sucursal desea usar el usuario para consultar productos o realizar sus compras
     public String elegirSucursal(){
         System.out.println("Que sucursal desea utilizar?: (Sucursal 1, Sucursal 2, Sucursal 3)");   
         int i = 10;
@@ -44,7 +44,7 @@ public class Vista {
         }
         return suc;
     }
-
+//Preguntar que producto desea probar 
     public int escogerProducto(ArrayList<String[]> prodSucursal){
         System.out.println("\nQue producto dese utilizar?: ");
         for(int s = 0; s<prodSucursal.size();s++){
@@ -69,7 +69,7 @@ public class Vista {
 
         return Integer.parseInt(selec);
     } 
-
+//Preguntar si se desea probar el producto o añadir directamente al carrito
     public int preCarrito(){
         System.out.println("\n Que desea hacer?: \n1) Agregar directamente al carrito.\n2) Probar");
 
@@ -90,8 +90,7 @@ public class Vista {
 
         return Integer.parseInt(selec);
     }
-
-
+//Menú principal
     public int menu(){
         System.out.println("Que desea hacer?: \n1)Agregar producto\n2)Ver carrito\n3)Comprar todo en carrito y cerrar");
         int b = 10;
@@ -111,7 +110,7 @@ public class Vista {
         return Integer.parseInt(selec);
     }
 
-
+//Funciones del Smartphone para probarlo
     public int probarSmarPhone(){
         System.out.println("Que desea probar del Samrtphone?: \n1) Hacer llamada\n2) Verificar portabilidad\n3) Reproducir video\n4) Navegar por internet\n5) Tomar foto\n6) Terminar de probar");
         int b = 10;
@@ -131,12 +130,162 @@ public class Vista {
 
         return Integer.parseInt(selec);
     }
+//Funciones del Telefono Celular probarlo
+    public int probarTelefonoCelular(){
+        System.out.println("Que desea probar del Telefono Celular?: \n1) Hacer llamada\n2) Verificar portabilidad\nTerminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
 
+        while(b !=0){
+            pr = sc.nextLine();
 
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones del Telefono Fijo
+    public int probarTelefonoFijo(){
+        System.out.println("Que desea probar del Telefono Fijo?: \n1)Hacer llamada\n2)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")||pr.equals("4")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones de Camara fotografica
+    public int probarCamara(){
+        System.out.println("Que desea probar de la Camara Fotografica?: \n1)Tomar Fotografia\n2)Reproducir video\n3)Verificar portabilidad\n4)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")||pr.equals("4")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones del computador personal (Desk)
+    public int probarDesk(){
+        System.out.println("Que desea probaar del computador personal desk?: \n1)Navegar por internet\n2)Reproducir videos\n3)Ejecutar Juegos\n4)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")||pr.equals("4")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones del computador personal (Laptop)
+    public int probarLap(){
+        System.out.println("Que desea probaar del computador personal Laptop?: \n1)Navegar por internet\n2)Reproducir videos\n3)Ejecutar Juegos\n4)Comprobar portabilidad\n5)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")||pr.equals("4")||pr.equals("5")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones de la Smart TV
+    public int probarSmartTV(){
+        System.out.println("Que desea probaar del computador personal Laptop?: \n1)Navegar por internet\n2)Reproducir videos\n3)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones Tablets
+    public int probarTablet(){
+        System.out.println("Que desea probaar de la tablet?: \n1)Navegar por internet\n2)Reproducir videos\n3)Tomar Fotografia\n4)Verificar portabilidad\n5)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")||pr.equals("4")||pr.equals("5")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+        }
+        return Integer.parseInt(selec);
+    }
+//Funciones smartwatch
+    public int probarSmartW(){
+        System.out.println("Que desea probaar del smartwatch?: \n1)Tomar Fotografia\n2)Verificar portabilidad\n3)Hacer llamada\n4)Terminar de probar");
+        int b = 10;
+        String pr = "";
+        String selec = "";
+
+        while(b !=0){
+            pr = sc.nextLine();
+
+            if (pr.equals("1")||pr.equals("2")||pr.equals("3")||pr.equals("4")){
+                b = 0;
+                selec = pr;
+            }else{
+                System.out.println("\n Ingrese una opcion correcta");
+            }
+    }
+    return Integer.parseInt(selec);
+}
     public void emitirMensaje(String txt){
         System.out.println(txt);
     }
-    
+ //Preguntar si desea agregar el producto al carrito despues de probarlo   
     public int agregarAcarrito(){
         System.out.println("Luego de probar: Desea agregar el producto al carrito?: \n1)Si\n2)No");
         int b = 10;
